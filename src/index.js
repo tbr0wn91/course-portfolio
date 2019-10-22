12c3aabe0583cd6bea0bab20244d2d3be2251fd3
thomas brown <thomasbrown9101@gmail.com>
2019-10-22 14:40:16 -0700

    built aboutpage

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import { render } from "react-dom";
+import HomePage from "./Components/HomePage";
+import AboutPage from "./Components/AboutPage";
+
+render(<AboutPage />, document.getElementById("root"));
